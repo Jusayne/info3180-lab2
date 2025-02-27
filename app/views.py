b56835be3411437061f1f06e83477b32835f8c35
Jusayne Chambers <jusayne123@gmail.com>
2025-02-27 13:02:42 -0500

    lab 2 complete

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -11,11 +11,15 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    date_joined = ('2025-02-27')
+    return render_template('profile.html', formatted_date = format_date_joined(date_joined))
 
 
 ###
@@ -45,3 +49,8 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def format_date_joined(date_joined):
+    datej = datetime.datetime.strptime(date_joined, '%Y-%m-%d').date()
+    formatted_date = datej.strftime('%B, %Y')
+    return formatted_date
